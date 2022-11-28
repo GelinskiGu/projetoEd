a496b76af8990b6b173104e0c318dc0fec233ec3
Gustavo Gelinski <gustavogelinski@alunos.utfpr.edu.br>
2022-11-28 19:25:04 -0300

    selecoes.txt e figurinhas_total.txt encadeadas.

--- albumCopa.c
+++ albumCopa.c
@@ -1,3 +1,158 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct figurinha
+{
+    int cod_selecao;
+    char selecao[20]; // pais
+    int numero_jogador;
+    char nome[20];
+} TFig;
+
+typedef struct selecoes
+{
+    int cod_selecao;
+    char selecao[20];
+    struct selecoes *prox;
+} TSelecao;
+
+typedef struct figurinhaJogador
+{
+    int cod_selecao;
+    int chave;
+    char nome[20];
+    struct figurinhaJogador *prox;
+} TJogador;
+
+typedef struct album
+{
+    struct album *prox;
+    TFig figurinha;
+    TFig *fig;
+} TAlbum;
+
+typedef struct cabecaAlbum
+{
+    TAlbum *inicio;
+    TAlbum *fim;
+} TCabeca;
+
+TCabeca *criaCabecaAlbum();
+TSelecao *alocaSelecao(FILE *parquivo);
+TJogador *alocaFigurinhaJogador(FILE *parquivo);
+void lerSelecoes(FILE *parquivo, TSelecao **prim);
+void lerFigurinhasJogadores(FILE *parquivo, TJogador **prim);
+void imprimeListaSelecoes(TSelecao *p);
+void imprimeListaFigurinhasJogadores(TJogador *p);
+
+int main()
+{
+    FILE *arquivoSelecao, *arquivoFigurinhaJogador;
+    arquivoSelecao = fopen("selecoes.txt", "r");
+    arquivoFigurinhaJogador = fopen("figurinhas_total.txt", "r");
+    TSelecao *primSelecao = NULL;
+    TJogador *primJogador = NULL;
+    while (!feof(arquivoSelecao))
+        lerSelecoes(arquivoSelecao, &primSelecao);
+    fclose(arquivoSelecao);
+    imprimeListaSelecoes(primSelecao);
+    while (!feof(arquivoFigurinhaJogador))
+        lerFigurinhasJogadores(arquivoFigurinhaJogador, &primJogador);
+    fclose(arquivoFigurinhaJogador);
+    imprimeListaFigurinhasJogadores(primJogador);
+}
+
+TCabeca *criaCabecaAlbum()
+{
+    TCabeca *novo = NULL;
+    novo = (TCabeca *)malloc(sizeof(TCabeca));
+    if (!novo)
+        return NULL;
+    novo->inicio = NULL;
+    novo->fim = NULL;
+    return novo;
+}
+
+TSelecao *alocaSelecao(FILE *parquivo) // Cria e retorna um nó TSelecao
+{
+    TSelecao *novo = NULL;
+    novo = (TSelecao *)malloc(sizeof(TSelecao));
+    if (!novo)
+        return NULL;
+    fscanf(parquivo, "%d %s\n", &novo->cod_selecao, novo->selecao);
+    novo->prox = NULL;
+    return novo;
+}
+
+TJogador *alocaFigurinhaJogador(FILE *parquivo) // Cria e retorna um nó TJogador
+{
+    TJogador *novo = NULL;
+    novo = (TJogador *)malloc(sizeof(TJogador));
+    if (!novo)
+        return NULL;
+    fscanf(parquivo, "%d %d %s\n", &novo->cod_selecao, &novo->chave, novo->nome);
+    novo->prox = NULL;
+    return novo;
+}
+
+void lerSelecoes(FILE *parquivo, TSelecao **prim) // Insere no final um TSelecao na lista encadeada
+{
+    if (!(*prim))
+        *prim = alocaSelecao(parquivo);
+    else
+    {
+        TSelecao *novo = NULL, *aux = NULL;
+        aux = *prim;
+        while (aux->prox)
+            aux = aux->prox;
+        novo = alocaSelecao(parquivo);
+        aux->prox = novo;
+    }
+}
+
+void lerFigurinhasJogadores(FILE *parquivo, TJogador **prim) // Insere no final um TJogador na lista encadeada
+{
+    if (!(*prim))
+        *prim = alocaFigurinhaJogador(parquivo);
+    else
+    {
+        TJogador *novo = NULL, *aux = NULL;
+        aux = *prim;
+        while (aux->prox)
+            aux = aux->prox;
+        novo = alocaFigurinhaJogador(parquivo);
+        aux->prox = novo;
+    }
+}
+void imprimeListaSelecoes(TSelecao *p) // Printa a lista encadeada de 'seleções'
+{
+    if (p)
+    {
+        while (p)
+        {
+            printf("%d %s\n", p->cod_selecao, p->selecao);
+            p = p->prox;
+        }
+    }
+    else
+    {
+        printf("\nPosto vazio!\n");
+    }
+}
+
+void imprimeListaFigurinhasJogadores(TJogador *p) // Printa a lista encadeada de 'figurinhas_total'
+{
+    if (p)
+    {
+        while (p)
+        {
+            printf("%d %d %s\n", p->cod_selecao, p->chave, p->nome);
+            p = p->prox;
+        }
+    }
+    else
+    {
+        printf("\nPosto vazio!\n");
+    }
+}
